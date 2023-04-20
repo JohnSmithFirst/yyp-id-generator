2599134c62945bc1394205fab7201887910463d8
yangyiping1 <yangyiping1@jd.com>
2023-04-20 20:51:48 +0800

    初始化

---
+++ src/main/java/com/yyp/id/generator/IdGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.yyp.id.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yyp/id/generator/IdGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyp.id.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
